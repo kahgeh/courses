08563a6fffa586361078ad7b4168fe9a4de0018e
dev-kahgehtan <kahgeh.tan@xero.com>
2017-10-31 22:00:52 +1100

    update lesson 1 script and userdata to start from home/ubuntu

--- setup/Lesson1Setup.sh
+++ setup/Lesson1Setup.sh
@@ -3,3 +3,13 @@ wget https://raw.githubusercontent.com/fastai/courses/master/deeplearning1/nbs/l
 wget https://raw.githubusercontent.com/fastai/courses/master/deeplearning1/nbs/utils.py
 wget http://files.fast.ai/files/dogscats.zip
 wget https://raw.githubusercontent.com/fastai/courses/master/deeplearning1/nbs/vgg16.py
+
+mkdir data    
+mv dogscats.zip ./data    
+cd data
+unzip -q dogscats.zip
+rm dogscats.zip
+pip install kaggle-cli
+
+cd ..
+jupyter notebook

6306e56819e9c3d695023eab3cf45f66f70f8824
dev-kahgehtan <kahgeh.tan@xero.com>
2017-10-31 21:35:02 +1100

    provisioning spot instances for fastai

---
+++ setup/Lesson1Setup.sh
@@ -0,0 +1,5 @@
+cd nbs
+wget https://raw.githubusercontent.com/fastai/courses/master/deeplearning1/nbs/lesson1.ipynb
+wget https://raw.githubusercontent.com/fastai/courses/master/deeplearning1/nbs/utils.py
+wget http://files.fast.ai/files/dogscats.zip
+wget https://raw.githubusercontent.com/fastai/courses/master/deeplearning1/nbs/vgg16.py
